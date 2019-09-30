5daf4689d18da3e2df0f3313b5daae7d5f1cb2d4
yuanbp <yuanbp0912@163.com>
2019-09-30 12:54:26 +0800

    first commit

--- src/main/java/org/chieftain/dynamicds/annotation/DataSource.java
+++ src/main/java/org/chieftain/dynamicds/annotation/DataSource.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface DataSource {
